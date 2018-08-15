b1c113b9f5fc7e33282e4fbbb5c63a17425e4f0f
zhoudagang <15371646664@163.com>
2018-08-15 13:44:15 +0800

    页面输出hello world

--- cheung/cheung-base/src/main/java/org/cheung/base/controller/HelloController.java
+++ cheung/cheung-base/src/main/java/org/cheung/base/controller/HelloController.java
@@ -8,6 +8,9 @@
 */  
 package org.cheung.base.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +33,13 @@ public class HelloController {
 	return "【ECHO】" + msg ;
 	}
 
-	
+	@RequestMapping("/object")
+	public String object(HttpServletRequest request,HttpServletResponse response) {
+	System.out.println("*** 客户端IP地址：" + request.getRemoteAddr());
+	System.out.println("*** 取得客户端响应编码：" + response.getCharacterEncoding());
+	System.out.println("*** 取得SessionID：" + request.getSession().getId());
+	System.out.println("*** 取得真实路径：" + request.getServletContext().getRealPath("/upload/"));
+	return "object" ;
+	}
 	
 }
